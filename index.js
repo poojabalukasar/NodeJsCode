be2435f3b93043e5ed07075afa5d2fb3e6089307
poojabalukasar <poojakasar100@gmail.com>
2023-10-23 22:20:30 +0530

    question no.2 code added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+// Assignment Question
+
+// Q.1
+// console.log("hello world");
+
+//Q.2
+let productOfTwoNumbers = (num1, num2) => num1 * num2;
+console.log(productOfTwoNumbers(5, 5));
+
+//student object
+
+const student = {
+  name: "pooja",
+  age: 24,
+  marks: 99,
+  printInfo() {
+    console.log(
+      `Student name is ${this.name}, age is ${this.age} and marks is ${this.marks}`
+    );
+  },
+};
+
+console.log(student);
+student.printInfo();
