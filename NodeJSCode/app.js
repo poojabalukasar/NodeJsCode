881a14c9b69bf1d1a530e53b6c1fc5f1fa70b252
poojabalukasar <poojakasar100@gmail.com>
2023-10-27 13:49:02 +0530

    question no.8 code added

--- NodeJSCode/app.js
+++ NodeJSCode/app.js
@@ -1,5 +1,28 @@
 // Assignment Questions
 
+
+//Q.9
+// Did you understand event loop ? Please explain it in your words if you did.
+//Answer : yes,I understand event loop.
+//the job of the event loop is continuesly checking the call stack and callback queues if 
+//call stack is empty and any callback function are waiting inside the callback queue then push the callback function
+//inside the call stack for the execution. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Q.10
 
 // Install the nodemon package. What problem does nodemon solve?
@@ -9,3 +32,6 @@
 //When would you use --save-dev and --save?
 //Answer : Use --save-dev to save development dependencies
 //and --save to save runtime dependencies.
+
+
+
--- app.js
+++ app.js
@@ -2,24 +2,118 @@
 
 // Q.8
 
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.write("<html>");
+//     res.write("<head><title>Enter Message</title></head>");
+//     res.write(
+//       '<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Send</button></form></body>'
+//     );
+//     res.write("</html>");
+//     return res.end();
+//   }
+//   if (url === "/message" && method === "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       console.log(chunk);
+//       body.push(chunk);
+//     });
+//     req.on("end", () => {
+//       const parseBody = Buffer.concat(body).toString();
+//       const message = parseBody.split("=")[1];
+//       console.log(message);
+//       fs.writeFileSync("message.txt", message, (error) => {
+//         if (error) {
+//           console.log(error);
+//         }
+//       });
+//     });
+
+//     res.statusCode = 302;
+//     res.setHeader("location", "/");
+//     return res.end();
+//   }
+// });
+
+// server.listen(4000, () => {
+//   console.log("Server is running on port 4000");
+// });
+
+// What is the use of having buffers and streams? When would you use it?.
+//Answer :
+//Buffers: we use buffers when we need to work with binary data, such as
+// reading or writing files.
+//Buffers are especially useful when we want to perform operations on data in chunks, like encryption or compression,
+// before saving it to a file or sending it over the network.
+
+//Streams: Streams are valuable for processing data in a continuous and non-blocking manner.
+//They are crucial in situations where we want to read or write data in small chunks,
+// making them ideal for handling large files, streaming media (audio or video), processing real-time data
+// from sources like sensors or log files, and handling network communication with minimal memory usage and improved performance.
+
+//Chat app demo
+
 const http = require("http");
 const fs = require("fs");
+
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"> <input type="text" name="message"><button>Submit</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
+    fs.readFile("message.txt", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html><body><h1>Internal Server Error</h1></body></html>");
+        return res.end();
+      }
+
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write("<body><h2>Messages:</h2><div>");
+
+      data.split("\n").forEach((message) => {
+        if (message.trim() !== "") {
+          res.write(`<p>${message}</p>`);
+        }
+      });
+
+      res.write("</div>");
+      res.write(
+        '<form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
   }
+
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "new data added");
-    res.statusCode = 302;
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.appendFile("message.txt", message + "\n", (err) => {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.setHeader("Content-Type", "text/html");
+          res.write("<html><body><h1>Internal Server Error</h1></body></html>");
+          return res.end();
+        }
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 });
 
